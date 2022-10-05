5ecfc817f25bfaea5cac53b8c32c9b4adbda200a
Francesco-diPippa <dipippa.francescopio@gmail.com>
2022-10-05 16:12:15 +0200

    Implementazione classe Calcolatrice

---
+++ src/main/java/org/example/Calcolatrice.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Calcolatrice {
+    private int a;
+    private int b;
+
+    public Calcolatrice(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
